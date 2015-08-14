9c6f89b2f0153d94e140c7cb86c8bd10257975df
Luis Pabon <luis.pabon@rightster.com>
2015-08-14 08:48:15 +0100

    Add in skeleton test case

---
+++ tests/PhpConsoleLogger/PhpConsoleLoggerTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace PhpConsoleLogger\Tests;
+
+class PhpConsoleLoggerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function foo()
+    {
+        self::assertTrue(true);
+    }
+}
