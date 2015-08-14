3f25f4f55b667a81e4bc3e058471922ea2a406bc
Luis Pabon <luis.pabon@rightster.com>
2015-08-14 09:38:35 +0100

    add first test

--- tests/PhpConsoleLogger/PhpConsoleLoggerTest.php
+++ tests/PhpConsoleLogger/PhpConsoleLoggerTest.php
@@ -1,13 +1,59 @@
 <?php
 namespace PhpConsoleLogger\Tests;
 
+use PhpConsoleLogger\Console\Logger;
+use Psr\Log\LogLevel;
+
 class PhpConsoleLoggerTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @test
+     * @var Logger|\PHPUnit_Framework_MockObject_MockObject
      */
-    public function foo()
+    private $logger;
+
+    public function setUp()
     {
-        self::assertTrue(true);
+        $this->logger = $this->getMock('PhpConsoleLogger\Console\Logger', ['output', 'format']);
+    }
+
+    /**
+     * @test
+     * @dataProvider validScalarValues
+     */
+    public function logAcceptsScalarValues($value)
+    {
+        $logLevel = LogLevel::INFO;
+        $message = (string) $value;
+
+        $this->logger
+            ->expects(self::once())
+            ->method('format')
+            ->with($logLevel, $value)
+            ->will(self::returnValue($message));
+
+        $this->logger
+            ->expects(self::once())
+            ->method('output')
+            ->with($message);
+
+        $this->logger->log($logLevel, $value);
+    }
+
+
+    /***** Data providers *****/
+
+    private $scalarValues = [
+        [19.8],
+        ['foo'],
+        [-8],
+        [null],
+        [''],
+        [false],
+        [true],
+    ];
+
+    public function validScalarValues()
+    {
+        return $this->scalarValues;
     }
 }
