193a204949c6e90e68ab1dac87658d52c3fa8642
Luis Pabon <luis.pabon@rightster.com>
2015-08-14 10:58:30 +0100

    Some more tests

--- tests/PhpConsoleLogger/PhpConsoleLoggerTest.php
+++ tests/PhpConsoleLogger/PhpConsoleLoggerTest.php
@@ -3,6 +3,7 @@ namespace PhpConsoleLogger\Tests;
 
 use PhpConsoleLogger\Console\Logger;
 use Psr\Log\LogLevel;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 class PhpConsoleLoggerTest extends \PHPUnit_Framework_TestCase
 {
@@ -39,6 +40,39 @@ class PhpConsoleLoggerTest extends \PHPUnit_Framework_TestCase
         $this->logger->log($logLevel, $value);
     }
 
+    /**
+     * @test
+     * @expectedException \Psr\Log\InvalidArgumentException
+     * @expectedExceptionMessage Message can only be a string, number or an object which can be cast to a string
+     */
+    public function logDoesntAcceptUnstringableObjects()
+    {
+        $this->logger->log(LogLevel::ALERT, new \stdClass());
+    }
+
+    /**
+     * @test
+     * @dataProvider validStringableObjects
+     */
+    public function logAcceptsStringableObjects($object)
+    {
+        $logLevel = LogLevel::INFO;
+        $message = (string) $object;
+
+        $this->logger
+            ->expects(self::once())
+            ->method('format')
+            ->with($logLevel, $object)
+            ->will(self::returnValue($message));
+
+        $this->logger
+            ->expects(self::once())
+            ->method('output')
+            ->with($message);
+
+        $this->logger->log($logLevel, $object);
+    }
+
 
     /***** Data providers *****/
 
@@ -55,5 +89,21 @@ class PhpConsoleLoggerTest extends \PHPUnit_Framework_TestCase
     public function validScalarValues()
     {
         return $this->scalarValues;
+    }
+
+    public function validStringableObjects()
+    {
+        return [
+            [new stringableClass()],
+            [new Exception()],
+        ];
+    }
+}
+
+class stringableClass
+{
+    public function __toString()
+    {
+        return 'foo';
     }
 }
